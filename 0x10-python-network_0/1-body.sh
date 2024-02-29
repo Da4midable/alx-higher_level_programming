71c60b5c46d67662146962a94247aba33d904993
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-02-29 06:00:02 -0800

    1 fixed

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -3,8 +3,8 @@
 
 url=$1
 
-response=$(curl -s -o /dev/null -w "%{http_code}" $url)
+response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
 
-if [ $response -eq 200 ]; then
-    curl -s $url
+if [ "$response" -eq 200 ]; then
+    curl -s "$url"
 fi
