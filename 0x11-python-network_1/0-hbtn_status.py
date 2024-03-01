6f1b9c6356b0d41605302f992e6c5653d1a45eb5
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-03-01 08:37:37 -0800

    fetch https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Script Name: 0-hbtn_status.py
+Description:
+    This script sends a GET request to https://alx-intranet.hbtn.io/status
+    using the urllib.request module. It then prints information about response
+    body, including its type, raw content, and decoded content in UTF-8 format
+
+Shebang:
+    The shebang line '#!/usr/bin/python3' at the beginning of script indicates
+    that the script should be executed using Python 3 interpreter located at
+    '/usr/bin/python3'.
+
+Modules Used:
+    - urllib.request: Used to send HTTP requests and handle responses.
+
+Functionality:
+    1. Imports the 'urllib.request' module.
+    2. Defines URL to send request to: 'https://alx-intranet.hbtn.io/status'.
+    3. Sends a GET request to specified URL using 'urllib.request.urlopen()'.
+    4. Reads the response body using the 'read()' method.
+    5. Prints information about the response body:
+        - Type of the content (bytes)
+        - Raw content (bytes)
+        - Content decoded as UTF-8
+
+Example Output:
+    Body response:
+        - type: <class 'bytes'>
+        - content: b'OK'
+        - utf8 content: OK
+"""
+import urllib.request
+
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    html_content = response.read()
+
+print("Body response:")
+print("\t- type:", type(html_content))
+print("\t- content:", html_content)
+print("\t- utf8 content:", html_content.decode('utf-8'))
