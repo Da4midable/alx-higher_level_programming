34868325aa7e9c915fa65dcababfb7d79699fa41
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-03-01 09:41:35 -0800

    0&1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -39,6 +39,6 @@ with urllib.request.urlopen(url) as response:
     html_content = response.read()
 
 print("Body response:")
-print("\t- type:", type(html_content))
-print("\t- content:", html_content)
-print("\t- utf8 content:", html_content.decode('utf-8'))
+print("    - type:", type(html_content))
+print("    - content:", html_content)
+print("    - utf8 content:", html_content.decode('utf-8'))
