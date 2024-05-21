9744d52cb938258c3080faa13e596a89f0195b8e
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-05-21 20:46:50 +0100

    Star Wars API Scrapping

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const request = require('request');
+
+const movieID = process.argv[2];
+
+if (!movieID) {
+  console.error('Please provide a movie ID as the first argument');
+  process.exit(1);
+}
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error making the request:', error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Failed to fetch data. Status code:', response.statusCode);
+    process.exit(1);
+  }
+
+  const data = JSON.parse(body);
+
+  console.log(data.title);
+});
