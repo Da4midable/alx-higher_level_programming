9ac821035a46bc5b4dd708e3f4a5de1ca0a836b2
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-05-21 20:14:34 +0100

    GET scrapping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const filePath = process.argv[2]
-const text = process.argv[3]
+const filePath = process.argv[2];
+const text = process.argv[3];
 
-const fs = require('fs')
+const fs = require('fs');
 
 fs.writeFile(filePath, text, (err) => {
   if (err) {
-    console.error(err)
+    console.error(err);
   }
-})
+});
