c5d64073653bdef0715165b5935233e10c2421c3
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-07-10 13:26:46 +0100

    100 added again

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print(''.join([chr(i) + chr(i+1).lower() for i in range(65, 91, 2)])[::-1], end="")
+print('{0}'.format(''.join([chr(i) + chr(i+1).lower() for i in range(65, 91, 2)])[::-1]), end="")
